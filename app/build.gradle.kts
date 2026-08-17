f6a4c2d5754da3476725efa1b13949dbd3c77f7a
Inigo Lacoume <inigo.lacoume@picsart.com>
2026-08-17 18:34:46 +0200

    update imports

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.inlacou.lib.jsonpathapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.inlacou.lib.jsonpathapp"
